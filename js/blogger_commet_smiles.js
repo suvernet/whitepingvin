1c8cdd7ed15e0ccb5c9d2275e20dffcd6a7f6803
suvernet <74303408+suvernet@users.noreply.github.com>
2021-01-29 06:44:11 +0300

    Update blogger_commet_smiles.js

--- js/blogger_commet_smiles.js
+++ js/blogger_commet_smiles.js
@@ -8,18 +8,18 @@ if(document.getElementsByClassName == undefined) {
 for (key in smiles) {
   var smile = smiles[key]
   if (smile.innerHTML) {
-    smile.innerHTML = smile.innerHTML.replace(/(:-?|=)\)/ig, p+"smile_mini.gif"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(:-?|=)\(|(:nothappy:)/ig, p+"sad_mini.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(:-?|=)\)|(:smile:)/ig, p+"smile_mini.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(:-?|=)\(|(:sad:)/ig, p+"sad_mini.gif"+u);
     smile.innerHTML = smile.innerHTML.replace(/(;)\)|(:wink:)/ig, p+"wink_mini.gif"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(:z)|(:ne-a:)/ig, p+"0B3_dGXEjBVdPendKYVdYUGMyVUk"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(:D)|(:big_smile:)/g, p+"0B3_dGXEjBVdPVjZ2NklUMHVjd3c"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(8-0)|(:boom:)/ig, p+"0B3_dGXEjBVdPaEUtLXNkRlRZcVE"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(:p)|(:tease:)/ig, p+"0B3_dGXEjBVdPa1E2Y3NLU0dkWUk"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(%;)|(:cry:)/ig, p+"0B3_dGXEjBVdPT3pmaHZkUEFTWFU"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(XD)|(:lol:)/g, p+"0B3_dGXEjBVdPZmtUenZORDlqRms"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(:z)|(:ne-a:)/ig, p+"nea_mini.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(:D)|(:biggrin:)/g, p+"biggrin_mini2.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(8-0)|(:dirol:)/ig, p+"dirol_mini.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(:p)|(:blum:)/ig, p+"blum_mini.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(%;)|(:cray:)/ig, p+"cray_mini.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(XD)|(:lol:)/g, p+"lol_mini.gif"+u);
     smile.innerHTML = smile.innerHTML.replace(/(_@)|(:pardon:)/ig, p+"0B3_dGXEjBVdPanJLakJPdFhWTlE"+u);
-    smile.innerHTML = smile.innerHTML.replace(/:crazy:/g, p+"0B3_dGXEjBVdPOERQWXNUREZUT0U"+u);
-    smile.innerHTML = smile.innerHTML.replace(/(O_o)|(:tik:)/g, p+"0B3_dGXEjBVdPbm4zdjVqV1I5TU0"+u);
+    smile.innerHTML = smile.innerHTML.replace(/:wacko:/g, p+"wacko_mini2.gif"+u);
+    smile.innerHTML = smile.innerHTML.replace(/(O_o)|(:chok:)/g, p+"chok_mini.gif"+u);
     smile.innerHTML = smile.innerHTML.replace(/:beer:/g, p+"0B3_dGXEjBVdPRDA0NmtESkcxcWs"+u);
     smile.innerHTML = smile.innerHTML.replace(/:friends:/g, p+"0B3_dGXEjBVdPekFjWmFoVXpZWTA"+u);
     smile.innerHTML = smile.innerHTML.replace(/(:help:)|(:sos:)/g, p+"0B3_dGXEjBVdPNE1XdzNnS1B6Rjg"+u);
