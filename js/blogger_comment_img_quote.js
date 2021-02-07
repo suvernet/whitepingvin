2848b6ccb92d1b6a6af0c1966742f1c6cf6b38e7
suvernet <74303408+suvernet@users.noreply.github.com>
2021-02-08 01:32:31 +0300

    Update blogger_comment_img_quote.js

--- js/blogger_comment_img_quote.js
+++ js/blogger_comment_img_quote.js
@@ -17,9 +17,9 @@ if (c) {
 function replaceneechal() {
     _str = _str['replace'](/\гЂ€/gi, '<');
     _str = _str['replace'](/\гЂ‰/gi, '>');
-    _str = _str['replace'](/\[img\]/gi, '<img src=\'');
-    _str = _str['replace'](/\[img\#\]/gi, '<img width=\'100%\' src=\'');
-    _str = _str['replace'](/\[\/img\]/gi, '\' class=\'image\'/>');
+    _str = _str['replace'](/\[im\]/gi, '<img src=\'');
+    _str = _str['replace'](/\[im\#\]/gi, '<img width=\'100%\' src=\'');
+    _str = _str['replace'](/\[\/im\]/gi, '\' class=\'image\'/>');
 
     _str = _str['replace'](/(\[q=\")|(\[url\])/gi, '<table><tr><td><span class=comment-quote-name><span class=name-color>');
     _str = _str['replace'](/(\"\])|(\[\/url\]\[q\])/gi, '</span> писал\(а\)</span></td></tr></table><table class=comment-quote><tr><td>');
